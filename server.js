1ffca91557ee312de34f8519cc623ef18489d419
eomseung-yeol <co6747@naver.com>
2022-06-30 16:14:33 +0900

    add read me

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
- // 8080 local port에 node server를 오픈한다.
+// 8080 local port에 node server를 오픈한다.s
 app.listen(8080, () => {
-    console.log('listen on 8080');
+  console.log("listen on 8080");
 });
 
-app.set('views',__dirname+'/views'); // 루트에 render라고 정의되어 있는 구문을 View라는 이름을 가지고 있는 폴더값을 기반으로 가져온다. 
-app.set('view engine','ejs');  // ejs 엔진을 기반으로 node 를 동작시킨다. ejs=> Embedded JavaScrip.   
-app.engine('html',require('ejs').renderFile);
+app.set("views", __dirname + "/views"); // 루트에 render라고 정의되어 있는 구문을 View라는 이름을 가지고 있는 폴더값을 기반으로 가져온다.
+app.set("view engine", "ejs"); // ejs 엔진을 기반으로 node 를 동작시킨다. ejs=> Embedded JavaScrip.
+app.engine("html", require("ejs").renderFile);
 
 /*
  node server 단에서, get 요청 방식
@@ -18,30 +17,26 @@ app.engine('html',require('ejs').renderFile);
  render() => 전송하고자 하는 파일을 정의할 때, 다음과 같이 지정한다. 
 */
 
-// '/' => 루트 경로를 메인에 띄우는 역할. 메인 접속시 나오는 화면을 정의할 때 '/'로 정의한다. 
+// '/' => 루트 경로를 메인에 띄우는 역할. 메인 접속시 나오는 화면을 정의할 때 '/'로 정의한다.
 //test2
-app.get('/', (req, res) => {
-    // res.send('hello world');
-    res.render('index.html');
-}
-);
-
-app.get('/about', (req, res) => {
-    // res.send('hello world');
-    res.render('about.html');
-}
-);
-app.get('/test', (req, res) => {
-    // res.send('hello world');
-    res.render('test.html');
-}
-);
-
-app.get('/test',(req, res) =>{
-    res.send('get  test');
+app.get("/", (req, res) => {
+  // res.send('hello world');
+  res.render("index.html");
 });
 
-app.get('/test1',(req, res)=>{
-    res.send('get  test1');
+app.get("/about", (req, res) => {
+  // res.send('hello world');
+  res.render("about.html");
+});
+app.get("/test", (req, res) => {
+  // res.send('hello world');
+  res.render("test.html");
 });
 
+app.get("/test", (req, res) => {
+  res.send("get  test");
+});
+
+app.get("/test1", (req, res) => {
+  res.send("get  test1");
+});
